29ffa6763941cc431445092383106170227e1615
Petri Pehkonen <petri.pehkonen@aidon.com>
2025-08-29 13:56:59 +0300

    added azure function

---
+++ GasProxyFunctions/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddHttpClient();
+        services.AddSingleton<GasProxyFunctions.Proxy.GasForwarder>();
+    })
+    .Build();
+
+await host.RunAsync();
