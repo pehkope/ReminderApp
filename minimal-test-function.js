55d965454fdd65f5b620b2eaa7bbb5a369723aa6
Petri Pehkonen <petri.pehkonen@aidon.com>
2025-09-04 20:30:19 +0300

    Update minimal test function message

---
+++ minimal-test-function.js
@@ -0,0 +1,16 @@
+module.exports = async function (context, req) {
+    context.log('Minimal test function triggered');
+
+    context.res = {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: {
+            success: true,
+            message: 'Minimal test function works!',
+            timestamp: new Date().toISOString(),
+            function: 'MinimalTest'
+        }
+    };
+};
