5c4f51f931300021a776e1bb1dc7b67c0dcd5992
Petri Pehkonen <petri.pehkonen@aidon.com>
2025-09-09 18:51:46 +0300

    Add HealthCheck function for .NET deployment testing

---
+++ ReminderApp.Functions/HealthCheck.cs
@@ -0,0 +1,45 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
+
+namespace ReminderApp.Functions;
+
+public class HealthCheck
+{
+    private readonly ILogger<HealthCheck> _logger;
+
+    public HealthCheck(ILogger<HealthCheck> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HealthCheck")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "health")] 
+        HttpRequestData req)
+    {
+        _logger.LogInformation("HealthCheck endpoint called");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "application/json");
+
+        var healthData = new
+        {
+            status = "healthy",
+            runtime = "dotnet-isolated",
+            timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+            version = "1.0.0",
+            message = ".NET Azure Functions is running"
+        };
+
+        var json = JsonSerializer.Serialize(healthData, new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        });
+
+        await response.WriteStringAsync(json);
+        return response;
+    }
+}
